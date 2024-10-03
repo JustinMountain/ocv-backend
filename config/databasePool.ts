f5b3790f82db0c1f9e439220e87aaeb9e028a20d
Justin Mountain <mountainj@gmail.com>
2024-10-03 18:45:13 +0000

    Updated to work with infrastructure container

--- config/databasePool.ts
+++ config/databasePool.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
   user: 'express_user',
-  host: process.env.DB_HOST || '127.0.0.1',
+  host: process.env.DB_HOST || '192.168.1.245',
   database: process.env.DB_NAME || 'our_civic_voice',
   password: process.env.DB_PASSWORD || 'password',
   port: 5432, // default PostgreSQL port
